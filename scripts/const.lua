a6bb47b6d06f2c5c7a898a57586dea9201ebfe5c
M4RT1LL0 <joseandres.alv@gmail.com>
2024-05-12 17:01:28 -0700

    Primer Commit
    ## Summary
    
    Primeros dos archivos: const y functions.
    
    Const contiene valores constantes utilizados a lo largo de Mudlet.
    
    Functions: Funciones para ser utilizados a lo largo de Mudlet.

---
+++ scripts/const.lua
@@ -0,0 +1,85 @@
+--[[
+
+         oooooooo8
+        o888     88   ooooooo  oo oooooo    oooooooo8
+        888         888     888 888   888  888ooooooo
+        888o     oo 888     888 888   888          888
+         888oooo88    88ooo88  o888o o888o 88oooooo88
+
+    Valores constantes para ser utilizados en Triggers, Timers, etc.
+    El contenido de este archivo debe ser puesto en algun script
+    dentro de Mudlet.
+]]--
+
+
+
+PATHS = {
+    catacumbas=".d",
+    campo_batalla=".2s3e4n2w4n3w",
+    ciudad_clanes=".2e",
+    escuela_mud=".u",
+    guarderia_enana=".ue",
+    yad=".uwd",
+    llanuras_norte=".2s3e4n2w3n",
+    bosque_midennir=".2s3e8s2w2s",
+    cementerio=".2s3e7sws",
+    bosque_haon_dor=".2s5w",
+    moria=".2s6e3n",
+    aldea_gnoma=".2s8es",
+    barrio_peligroso=".6se3se",
+    fabrica_mobs=".2s3w3se",
+    valle_elfos=".2s3e4n2w3n2enen",
+    arenas_desierto=".2s5e",
+    torre_wyvern=".2s6e4s2es2ede",
+    aracnolandia=".2s13ws2wnwuwn",
+    bosque_sagrado=".2s8en",
+    ruinas_thalos=".2s6e4s3w",
+    las_alcantarillas=".4sd",
+    torre_dragones=".2s13ws2w2sws3wnw2n",
+    canyon_elemental=".2s6e4s2es2eds",
+    ciudad_ofcol=".2s3e4n2w7n6e3n",
+    comarca=".2s5wn",
+    templo_loto_blanco=".6s2e3s2w6ses2e",
+    olimpo=".2s3e4n2w8nu",
+    catacumbas_enanas=".2s8w2n4wn2w2n2w5swn",
+    fortaleza_goblin=".6s2e3s2w7s2e5s",
+    reino_enano=".2s6e3ne",
+    antigua_thalos=".2s8e2n3e2n",
+    torreon_hechizos=".2s13ws2w2sws3wnw2n",
+    plano_astral=".s3u",
+    nueva_thalos=".2s15e",
+    templo_drow=".2s6e4s2es2edw",
+    viejo_pantano=".2s13ws2w2sw3s",
+    capilla=".2s3e7sw6s",
+    dylans_area=".6se3sws3u",
+    gondolin=".2s3e4n2w4n2w2n",
+    yggdrasil=".2s13ws2wnwuw4neus2eswsdnwneunds5wn",
+    galaxia=".2s13ws2w2sws3wnw2n",
+    atlantis=".2s35eded",
+    dragon_spyre=".2s3e4n2w7n7e",
+    valle_titanes=".2s8wn",
+    feria_noldor=".6s2e3s2w2s2e2se",
+    villa_ix=".6s2e3s2w7s2en",
+    castillo_rey=".6s2e3s2w2sw2s2ws2w",
+    guarida_darathorn=".2s3e4n2w7n5en",
+    infierno=".4sdwdwn3dswdw",
+    valle_tumladen=".2s3e4n2w4n2w2n2wn2wueu5n3w3n2u3n3w4n5e",
+    valle_hielo=".2s13wne",
+    teatro_encantado=".2s7w2swn",
+    templo_razas=".2s13w3n",
+    catedral_oscura=".2s6e4s3e",
+    abismo=".4sdwdwn3dswd6w2des2en2es2ene2dswn2ws4wn2w2d2esen3esen2e2s5d",
+    ciudadela_aligator=".2s3e8s2w2sw2s2ws7w",
+    ciudad_locura=".2s3w4n2e12n",
+    silverdale=".2s6e10n2e9n",
+    cavernas_trillium=".2s21e7sd",
+    casa_elrond=".2s3e4n2w7nwn2d",
+    isla_ghrammar=".2s21e5sd10e",
+    ramiug=".2s3e8s2w2sw2s2wsw2n2dn",
+    ciudad_amazona=".2s3e4n2w7n3en",
+    pantano_oscuro=".2s13ws2w2sw7s3es3w",
+    palacio_locura=".2s3w4n2e14n5w",
+    ciudad_fortificada=".2s21e8s",
+    templo_soledad=".2s21e10nd",
+    barco_fantasma=".2s34en2e"
+    }
---
+++ scripts/functions.lua
@@ -0,0 +1,79 @@
+--[[
+
+
+ooooooooooo                                    o8   o88
+ 888       oooo  oooo  oo oooooo    ooooooo  o888oo oooo   ooooooo  oo oooooo    oooooooo8
+ 888ooo8    888   888   888   888 888     888 888    888 888     888 888   888  888ooooooo
+ 888        888   888   888   888 888         888    888 888     888 888   888          888
+o888o        888o88 8o o888o o888o  88ooo888   888o o888o  88ooo88  o888o o888o 88oooooo88
+
+
+    Codigo para ser utilizado en Triggers, Timers, etc.
+    El contenido de este archivo debe ser puesto en algun script
+    dentro de Mudlet.
+
+]]--
+
+-- Helpers.
+
+function is_numeric(x)
+    --[[
+        Revisar si un string es un numero
+    ]]--
+    if tonumber(x) ~= nil then
+        return true
+    end
+    return false
+end
+
+-- Funciones.
+
+function stringPathToMovementCommands( path )
+
+    --[[
+        En Mudlet, cuando uno pone un path que empieza con ".", mudlet
+        envia el comando gritar en vez de ir al path. Mudlet tampoco
+        entiende paths numericos(e.j. 13n). Esta funcion transforma
+        tales paths en comandos de movimiento.
+
+        Basicamente existen tres tipos de paths:
+            * letra
+            * 1 digito 1 letra
+            * 2 digitos 1 letra
+
+        Para una version mas "flexible", mirar https://gist.github.com/martilllo/a2c9f341cb2c7431a01e320dac6fabac
+
+        Parametros:
+            * path: string. Path empezando con "."
+    ]]--
+
+    local i = 2
+    local num
+    local dir
+
+    while i <= #path
+    do
+
+        -- Obtener sub-paths
+        if not isNumeric(path:sub(i,i))
+        then
+          num = 1
+          dir = path:sub(i,i)
+          i = i + 1
+        elseif isNumeric(path:sub(i,i)) and not isNumeric(path:sub(i+1,i+1)) then
+          num = path:sub(i,i)
+          dir = path:sub(i+1,i+1)
+          i = i + 2
+        else
+          num = path:sub(i,i+2)
+          dir = path:sub(i+3,i+3)
+          i = i + 3
+        end -- termina if
+
+        -- Moverse
+        for j=1,num do
+          send(dir)
+        end
+
+    end -- termina while
+end -- termina funcion
